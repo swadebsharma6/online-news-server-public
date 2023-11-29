d12c8f1282b8f75bff7577f451b3af8535e3330d
swadebsharma6 <swadebriva2012@gmail.com>
2023-11-29 10:20:54 +0600

    added jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,6 +32,108 @@ async function run() {
     const trendingCollection = client.db("news12SMDB").collection("trandings");
     const demoNewsCollection = client.db("news12SMDB").collection("demo-news");
     const articleCollection = client.db("news12SMDB").collection("articles");
+    const viewsCollection = client.db("news12SMDB").collection("views");
+    const userCollection = client.db("news12SMDB").collection("users");
+
+    // jwt related api
+    app.post('/jwt', async(req, res)=>{
+         const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+        res.send({token})
+    })
+
+    // middleware
+    const verifyToken = async(req, res, next)=>{
+      // console.log('inside verify token', req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'Forbidden Access'})
+      }
+      const token = req.headers.authorization.split(' ')[1];
+     
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+          return res.status(401).send({message: 'Forbidden Access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+
+    };
+
+    const verifyAdmin = async(req, res, next)=>{
+        const email = req.decoded.email;
+        const query = {email: email}
+        const user = await userCollection.findOne(query);
+        const isAdmin = user?.role ==='admin';
+        if(!isAdmin){
+          return res.status(403).send({message: 'Forbidden Access'})
+        };
+        next();
+    }
+
+    // user Related Api
+    app.patch('/user/admin/:id', async(req, res)=>{
+        const id = req.params.id;
+        const  filter = {_id: new ObjectId(id)};
+        const updatedDoc ={
+          $set:{
+            role: 'admin'
+          }
+        }
+        const result = await userCollection.updateOne(filter,updatedDoc);
+        res.send(result)
+    })
+
+    app.get('/users', verifyToken, verifyAdmin, async(req, res)=>{
+      // console.log(req.headers)
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    });
+
+    // admin user api
+    app.get('/users/admin/:email', verifyToken, verifyAdmin, async(req, res)=>{
+        const email = req.params.email;
+        if(email !== req.decoded.email){
+          return res.status(403).send({message: 'unauthorized Access'})
+        }
+
+        const query ={email: email}
+        const user = await userCollection.findOne(query);
+       let admin = false;
+       if(user){
+        admin  = user?.role === 'admin'
+       }
+       res.send({admin})
+
+    })
+
+
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      const query ={email: user.email};
+      const existUser = await userCollection.findOne(query);
+      if(existUser){
+        return res.send({message: 'user already exist', insertedId: null})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+  
+    // views Collection
+    app.post('/views', async(req, res)=>{
+      const news = req.body;
+      const result = await viewsCollection.insertOne(news);
+      res.send(result);
+    });
+
+    app.get('/views', async(req, res)=>{
+      const email = req.query.email;
+      const query ={email: email};
+      const cursor = viewsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result)
+    })
 
     // post article
     app.post('/articles', async(req, res)=>{
@@ -42,6 +145,13 @@ async function run() {
     app.get('/articles', async(req, res)=>{
       const cursor =  articleCollection.find();
       const result =await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/articles/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result =await articleCollection.findOne(query);
       res.send(result)
     })
 
